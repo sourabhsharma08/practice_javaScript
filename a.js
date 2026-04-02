82b6d41ebaa382a4bfc13f3973ff46162927aeac
Sourbh <sourabhsharma7206317653@gmail.com>
2026-04-02 11:13:45 +0530

    test

---
+++ a.js
@@ -0,0 +1,45 @@
+// let n =5 
+
+// let result = ''
+// for(let i=0; i<n; i++){
+//     for(let j=0; j<n ; j++){
+//        result+='* ' 
+//     }
+//      result+='\n'
+// }
+// console.log(result);
+
+
+// let n =5
+
+// let result=''
+// for(let i=0; i<=n; i++){
+//     for(let j=1; j<=i; j++){
+//         result+=j
+//     }
+//     result+='\n'
+// }
+// console.log(result);
+
+
+// let n =5
+
+// let result=''
+// for(let i=0; i<=n; i++){
+//     for(let j=1; j<=i; j++){
+//         result+='*'
+//     }
+//     result+='\n'
+// }
+// console.log(result);
+
+let n =5
+
+let result=''
+for(let i=0; i<=n; i++){
+    for(let j=1; j<=i; j--){
+        result+='*'
+    }
+    result+='\n'
+}
+console.log(result);

3fdde23b5ce13b62144845fc3a0d2808ea662094
Sourbh <sourabhsharma7206317653@gmail.com>
2026-04-02 11:15:22 +0530

    commend

--- a.js
+++ a.js
@@ -33,13 +33,13 @@
 // }
 // console.log(result);
 
-let n =5
+// let n =5
 
-let result=''
-for(let i=0; i<=n; i++){
-    for(let j=1; j<=i; j--){
-        result+='*'
-    }
-    result+='\n'
-}
-console.log(result);
+// let result=''
+// for(let i=0; i<=n; i++){
+//     for(let j=1; j<=i; j--){
+//         result+='*'
+//     }
+//     result+='\n'
+// }
+// console.log(result);
